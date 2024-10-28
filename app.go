cc92f49f2372f8f8f6d414afa53b268d8bb68da7
Marco Orlandi <Gianluca1996@gmx.de>
2024-10-28 15:31:20 +0100

    push

--- app.go
+++ app.go
@@ -4,24 +4,34 @@ import (
 	"fmt"
 )
 
-func main(){
-var revenue,expenses,taxRate float64
+func main() {
+	// const geht auch wie gewohnt -> float64 ist der type
+	// var revenue, expenses, taxRate float64
 
-fmt.Print("Revenue Amount: ")
-fmt.Scan(&revenue)
+	revenue := getUserInput("Revenue Amount: ")
 
-fmt.Print("Expenses Amount: ")
-fmt.Scan(&expenses)
+	expenses := getUserInput("Expenses: ")
 
-fmt.Print("Tax Rate: ")
-fmt.Scan(&taxRate)
+	taxRate := getUserInput("Tax Rate: ")
 
-resultBeforeTax := revenue - expenses
-resultAfterTax := resultBeforeTax * (1 - taxRate/100)
-result := resultBeforeTax / resultAfterTax
+	resultBeforeTax, resultAfterTax, result := calcProfit(revenue, expenses, taxRate)
 
-// fmt.Println("Profit before tax: ", resultBeforeTax)
-// fmt.Println("Profit after tax: ", resultAfterTax)
-fmt.Printf("Profit before tax: %.1f\n Profit after tax: %.1f\n Result: %.1f",resultBeforeTax,resultAfterTax,result)
+	fmt.Printf("%.1f\n", resultBeforeTax)
+	fmt.Printf("%.1f\n", resultAfterTax)
+	fmt.Printf("%.1f\n", result)
+}
 
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calcProfit(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	resultBeforeTax := revenue - expenses
+	resultAfterTax := resultBeforeTax * (1 - taxRate/100)
+	result := resultBeforeTax / resultAfterTax
+
+	return resultBeforeTax, resultAfterTax, result
 }
